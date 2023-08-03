7028c18e3b6abd4d522aab2e2b6f26a3e16a198f
Hevsy <roomyn@gmail.com>
2023-08-03 22:42:22 +1000

    Introduced logging + refactoring

--- func.py
+++ func.py
@@ -1,24 +1,17 @@
+from datetime import datetime
+from tzlocal import get_localzone
+
 import re
 
 
-def log_error(message):
-    with open("error.log", "a") as log_file:
-        log_file.write(message + "\n")
-
-
-def parse_entry(entry):
-    parts = entry.split()
-    if len(parts) != 2:
-        return None, None
-    return parts
-
-
 def url_strip(s):
+    """Returns string with the url protocol and 'www' prefix stripped"""
     pat = r"^(http(s)?:\/\/)?(www\.)?"
     return re.sub(pat, "", s)
 
 
-def slash_strip(s):
+def slash_strip(s) -> str:
+    """Returns string with the trailing '/' stripped"""
     return s.rstrip("/")
 
 
@@ -28,11 +21,31 @@ def split_url(url):
     return site_id, path
 
 
-def parse_line(line):
-    url, site_category = parse_entry(line)
+def parse_line(line, delimiter=" "):
+    parts = line.strip().split(delimiter)
+
+    # Two parts required for parsing: url and site_category.
+    # If there are more or less parts in the line - it is considered malfored
+    # and function returns 'None' for all outputs, so if can later be flagged as invalid
+    if len(parts) != 2:
+        return None, None, None
+    url = parts[0]
+    site_category = parts[1]
     site_id, path = split_url(url)
     return site_category, site_id, path
 
 
-def invalid_output(site_category, site_id):
+def invalid_output(site_category, site_id) -> bool:
+    """Checks if both required parts exist: site_id and site_category"""
     return not site_id or not site_category
+
+
+def time_now(fmt) -> str:
+    """Returns current time in a specified format"""
+    return datetime.now(tz=get_localzone()).strftime(fmt)
+
+
+def timestamp() -> str:
+    """Returns current time in 'YYYYMMDD HH:MM TMZ' format"""
+    fmt = "%Y%m%d %H:%M %Z"
+    return time_now(fmt)
--- main.py
+++ main.py
@@ -1,23 +1,34 @@
-from func import log_error, parse_line, invalid_output
+import logging
+from func import parse_line, invalid_output, time_now, timestamp
+
+# from datetime import datetime
+# 
+# print(datetime.now(tz=get_localzone()).strftime('%Z'))
+# # CEST
 
 
 def main():
+    logging.basicConfig(
+        filename=f"import-error.{time_now('%Y%m%d')}.{time_now('%H%M%S')}.{time_now('%Z')}.log",
+        filemode="a",
+        format="%(message)s",
+    )
+
     with open("list1.txt", "r") as file:
         for line in file:
             site_category, site_id, path = None, None, None
             if not line:
                 continue  # Skip empty lines
-            line = line.strip()  # Remove leading/trailing whitespace
 
             site_category, site_id, path = parse_line(line)
 
             if invalid_output(site_category, site_id):
-                log_error("Invalid line: " + line)
+                logging.error(f"{timestamp()} Invalid line: {line}")
                 print("Error - skipping")  # for debugging
                 continue
 
             print(site_id, path, site_category, sep="\n")  # for debugging
-            print("__________________")  # for debugging
+            print("__________________")
 
 
 if __name__ == "__main__":
