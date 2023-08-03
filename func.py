25bc038bf5082cee0b9dee40af5a5808f235fc03
Hevsy <roomyn@gmail.com>
2023-08-03 17:49:07 +1000

    Refactoring and debugging

--- func.py
+++ func.py
@@ -1,8 +1,10 @@
 import re
 
+
 def log_error(message):
     with open("error.log", "a") as log_file:
         log_file.write(message + "\n")
+
 
 def parse_entry(entry):
     parts = entry.split()
@@ -10,14 +12,27 @@ def parse_entry(entry):
         return None, None
     return parts
 
+
 def url_strip(s):
     pat = r"^(http(s)?:\/\/)?(www\.)?"
     return re.sub(pat, "", s)
 
+
 def slash_strip(s):
     return s.rstrip("/")
 
+
 def split_url(url):
     url = url_strip(slash_strip(url))
-    site_id, *path = url.split('/')
-    return site_id,path
+    site_id, *path = url.split("/")
+    return site_id, path
+
+
+def parse_line(line):
+    url, site_category = parse_entry(line)
+    site_id, path = split_url(url)
+    return site_category, site_id, path
+
+
+def invalid_output(site_category, site_id):
+    return not site_id or not site_category
--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-from func import log_error, split_url, url_strip, slash_strip, parse_entry
+from func import log_error, parse_line, invalid_output
+
 
 def main():
     with open("list1.txt", "r") as file:
         for line in file:
-            line = line.strip()  # Remove leading/trailing whitespace
+            site_category, site_id, path = None, None, None
             if not line:
                 continue  # Skip empty lines
-            
-            url, site_category = parse_entry(line)
-            
-            if not url or not site_category:
-                log_error("Invalid line")
-                print("Error - skipping")
-                continue
-            
-            site_id, path = split_url(url)
-            
-            print(site_id, path, site_category, sep="\n")
-            print("__________________")
+            line = line.strip()  # Remove leading/trailing whitespace
 
+            site_category, site_id, path = parse_line(line)
+
+            if invalid_output(site_category, site_id):
+                log_error("Invalid line: " + line)
+                print("Error - skipping")  # for debugging
+                continue
+
+            print(site_id, path, site_category, sep="\n")  # for debugging
+            print("__________________")  # for debugging
 
 
 if __name__ == "__main__":
