2404ee7d297fdc6e25cd3a398417a3d01df6eb9b
Hevsy <roomyn@gmail.com>
2023-08-07 08:49:49 +1000

    adding docstrings

--- logging_config.py
+++ logging_config.py
@@ -4,8 +4,17 @@ from func import time_now, timestamp
 
 
 def setup_logging():
+    """
+    Configures the logging system with error and success handlers.
 
-    logging.basicConfig(level=logging.INFO, format="%(message)s")
+    This function sets up the logging system to log error messages and successfully parsed lines to separate log files.
+
+    Error messages are logged to a file named 'import-error.{timestamp}.log', and successfully parsed lines are logged to 'import-complete.{timestamp}.log'.
+
+    Both log files are opened in append mode ('a'), and log entries are formatted to include the timestamp and message.
+    """
+
+    # logging.basicConfig(level=logging.INFO, format="%(message)s")
 
     error_handler = logging.FileHandler(
         f"import-error.{time_now('%Y%m%d')}.{time_now('%H%M%S')}.{time_now('%Z')}.log",
@@ -13,7 +22,10 @@ def setup_logging():
     )
     error_handler.setLevel(logging.ERROR)
 
-    success_handler = logging.FileHandler(f"import-complete.{time_now('%Y%m%d')}.{time_now('%H%M%S')}.{time_now('%Z')}.log", mode="a")
+    success_handler = logging.FileHandler(
+        f"import-complete.{time_now('%Y%m%d')}.{time_now('%H%M%S')}.{time_now('%Z')}.log",
+        mode="a",
+    )
     success_handler.setLevel(logging.INFO)
 
     formatter = logging.Formatter(f"{timestamp()} %(message)s")
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
 
     with open("list1.txt", "r") as file:
         for line in file:
+            # Initialising data for each loop (otherwise lines with empty field getting values form the prvious line)
             site_category, site_id, path = None, None, None
             if not line:
                 continue  # Skip empty lines
