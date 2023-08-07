dccbfda93b1b43e1ec29596bcb1523128abe5426
Hevsy <roomyn@gmail.com>
2023-08-07 22:42:20 +1000

    trobleshooting logging

--- func.py
+++ func.py
@@ -1,32 +1,62 @@
 from datetime import datetime
 from tzlocal import get_localzone
-
 import re
 
-
 def url_strip(s):
-    """Returns string with the url protocol and 'www' prefix stripped"""
+    """
+    Returns a string with the URL protocol ('http' or 'https') and 'www' prefix stripped.
+
+    Args:
+        s (str): The input URL string.
+
+    Returns:
+        str: The input URL with protocol and 'www' prefix removed.
+    """
     pat = r"^(http(s)?:\/\/)?(www\.)?"
     return re.sub(pat, "", s)
 
-
 def slash_strip(s) -> str:
-    """Returns string with the trailing '/' stripped"""
+    """
+    Returns a string with the trailing '/' characters stripped.
+
+    Args:
+        s (str): The input string.
+
+    Returns:
+        str: The input string with trailing '/' characters removed.
+    """
     return s.rstrip("/")
 
-
 def split_url(url):
+    """
+    Splits a URL into site ID and path components after stripping protocol and 'www'.
+
+    Args:
+        url (str): The input URL string.
+
+    Returns:
+        tuple: A tuple containing site ID and path components of the URL.
+    """
     url = url_strip(slash_strip(url))
     site_id, *path = url.split("/")
     return site_id, path
 
-
 def parse_line(line, delimiter=" "):
+    """
+    Parses a line of text containing a URL and site category.
+
+    Args:
+        line (str): The input line to parse.
+        delimiter (str, optional): The delimiter used to split the line into parts. Defaults to space (' ').
+
+    Returns:
+        tuple: A tuple containing site category, site ID, and path components.
+    """
     parts = line.strip().split(delimiter)
 
-    # Two parts required for parsing: url and site_category.
-    # If there are more or less parts in the line - it is considered malfored
-    # and function returns 'None' for all outputs, so it can later be flagged as invalid
+    # Two parts required for parsing: URL and site category.
+    # If there are more or less parts in the line, it is considered malformed
+    # and the function returns 'None' for all outputs, indicating it can be flagged as invalid.
     if len(parts) != 2:
         return None, None, None
     url = parts[0]
@@ -34,19 +64,38 @@ def parse_line(line, delimiter=" "):
     site_id, path = split_url(url)
     return site_category, site_id, path
 
-
 def invalid_output(site_category, site_id) -> bool:
-    """Checks if both required parts exist: site_id and site_category.
-    If either one is missing, the output deemed invalid and function return 'True'"""
+    """
+    Checks if both required parts exist: site ID and site category.
+
+    Args:
+        site_category (str): The site category.
+        site_id (str): The site ID.
+
+    Returns:
+        bool: True if either site ID or site category is missing, indicating an invalid output.
+    """
     return not site_id or not site_category
 
-
 def time_now(fmt) -> str:
-    """Returns current time in a specified format"""
+    """
+    Returns the current time in a specified format.
+
+    Args:
+        fmt (str): The desired time format.
+
+    Returns:
+        str: The current time formatted according to the provided format.
+    """
     return datetime.now(tz=get_localzone()).strftime(fmt)
 
-
 def timestamp() -> str:
-    """Returns current time in 'YYYYMMDD HH:MM TMZ' format"""
+    """
+    Returns the current time in 'YYYYMMDD HH:MM TMZ' format.
+
+    Returns:
+        str: The current time formatted as 'YYYYMMDD HH:MM TMZ'.
+    """
     fmt = "%Y%m%d %H:%M %Z"
     return time_now(fmt)
+
--- logging_config.py
+++ logging_config.py
@@ -14,23 +14,27 @@ def setup_logging():
     Both log files are opened in append mode ('a'), and log entries are formatted to include the timestamp and message.
     """
 
-    # logging.basicConfig(level=logging.INFO, format="%(message)s")
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)  # Set logger's level to DEBUG
 
+    # Create a formatter
+    formatter = logging.Formatter(f"{timestamp()} | %(message)s")
+
+    # Create a file handler for error logs
     error_handler = logging.FileHandler(
         f"import-error.{time_now('%Y%m%d')}.{time_now('%H%M%S')}.{time_now('%Z')}.log",
         mode="a",
     )
-    error_handler.setLevel(logging.ERROR)
+    error_handler.setFormatter(formatter)
+    error_handler.setLevel(logging.ERROR)  # Set error handler's level to ERROR
+    logger.addHandler(error_handler)
 
+    # Create a file handler for successful logs
     success_handler = logging.FileHandler(
         f"import-complete.{time_now('%Y%m%d')}.{time_now('%H%M%S')}.{time_now('%Z')}.log",
         mode="a",
     )
-    success_handler.setLevel(logging.INFO)
-
-    formatter = logging.Formatter(f"{timestamp()} %(message)s")
-    error_handler.setFormatter(formatter)
+    success_handler.setLevel(logging.INFO)  # Set success handler's level to INFO
     success_handler.setFormatter(formatter)
+    logger.addHandler(success_handler)
 
-    logging.getLogger().addHandler(error_handler)
-    logging.getLogger().addHandler(success_handler)
