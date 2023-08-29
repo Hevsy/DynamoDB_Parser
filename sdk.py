238d0d00c4d2dc31db6a513569265131b229fdc9
Hevsy <roomyn@gmail.com>
2023-08-30 08:13:36 +1000

    WIP - dynamodb interface

--- sdk.py
+++ sdk.py
@@ -8,20 +8,20 @@ dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table(table_name)
 
 print(list(dynamodb.tables.all()))
+try:
+    # client.put_item(Item={"id": "34", "company": "microsoft"})
+    siteID = "google.co"
+    response = table.get_item(Key={"siteId": siteID})
+    print(response, "______________", response["Item"], end="\n")
+
+except ClientError as err:
+    print(err)
+
 # try:
 #     # client.put_item(Item={"id": "34", "company": "microsoft"})
 #     response2 = client.put_item(
 #         TableName="DynamoDB_parser-dev", Item={"siteId": {"S": "google.co"}}
 #     )
 #     print(response2)
-# except ClientError as err:
-#     print(err)
-
-# try:
-#     # client.put_item(Item={"id": "34", "company": "microsoft"})
-#     siteID = "google.co"
-#     response2 = client.get_item(TableName="DynamoDB_parser-dev", Key={"siteId": siteID})
-#     print(response2)
-
 # except ClientError as err:
 #     print(err)
