aeb2ce52e456b300909a6e835b20ffa786919fc0
Hevsy <roomyn@gmail.com>
2023-08-30 08:09:53 +1000

    WIP - updating boto3 clients to resources

--- sdk.py
+++ sdk.py
@@ -2,15 +2,26 @@ from urllib import response
 import boto3
 from botocore.exceptions import ClientError
 
-client = boto3.client("dynamodb")
+table_name = "DynamoDB_parser-dev"
 
-response = client.list_tables()
-print(response)
-try:
-    # client.put_item(Item={"id": "34", "company": "microsoft"})
-    response2 = client.put_item(
-        TableName="DynamoDB_parser-dev", Item={"siteId": {"S": "google.co"}}
-    )
-    print(response2)
-except ClientError as err:
-    print(err)
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(table_name)
+
+print(list(dynamodb.tables.all()))
+# try:
+#     # client.put_item(Item={"id": "34", "company": "microsoft"})
+#     response2 = client.put_item(
+#         TableName="DynamoDB_parser-dev", Item={"siteId": {"S": "google.co"}}
+#     )
+#     print(response2)
+# except ClientError as err:
+#     print(err)
+
+# try:
+#     # client.put_item(Item={"id": "34", "company": "microsoft"})
+#     siteID = "google.co"
+#     response2 = client.get_item(TableName="DynamoDB_parser-dev", Key={"siteId": siteID})
+#     print(response2)
+
+# except ClientError as err:
+#     print(err)
