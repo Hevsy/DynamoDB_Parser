632e7b3db82ce185dd80f83f1e3f86a893c412c8
Hevsy <roomyn@gmail.com>
2023-09-04 22:28:05 +1000

    documenting

--- record.py
+++ record.py
@@ -4,18 +4,45 @@ from func import parse_url, timestamp
 
 
 class Record:
+    """
+    Represents a record to be stored in DynamoDB.
+
+    Args:
+        site_id (str): The site ID.
+        site_categories (list): List of site categories.
+        key (str): The key for nesting levels.
+
+    Attributes:
+        site_id (str): The site ID.
+        site_categories (list): List of site categories.
+        key (str): The key for nesting levels.
+        is_invalid (bool): Indicates whether the record is invalid.
+        _data (dict or None): The nested dictionary structure representing the DynamoDB record.
+    """
+
     def __init__(self, site_id, path, comment, site_categories, key):
         self.site_id = site_id
         self.path = path
         self.comment = comment
         self.site_categories = site_categories
         self.key = key
-        self.is_invalid = True if not site_id or not site_categories else False
+        self.is_invalid = not (self.site_id and self.site_categories)
 
         self.data = self._create_record()
 
     @classmethod
     def from_line(cls, line, delimiter=" ", key="site"):
+        """
+        Create a Record instance from a line of text.
+
+        Args:
+            line (str): The input line to parse.
+            delimiter (str, optional): The delimiter used to split the line into parts. Defaults to space (' ').
+            key (str, optional): The key for nesting levels. Defaults to "site".
+
+        Returns:
+            Record: A Record instance created from the input line.
+        """
         parts = line.strip().split(delimiter)
 
         # Two parts required for parsing: URL and site category.
@@ -32,10 +59,10 @@ class Record:
 
     def _create_record(self):
         """
-        Create a nested dictionary structure representing a DynamoDB record.
+        Create the nested dictionary structure representing a DynamoDB record.
 
         Returns:
-            dict: The nested dictionary structure.
+            dict or None: The nested dictionary structure if valid, otherwise None.
         """
         if self.is_invalid:
             return None
@@ -54,7 +81,12 @@ class Record:
         return nested_structure
 
     def __str__(self):
-        # Override __str__ for debugging or logging
+        """
+        Get a string representation of the record.
+
+        Returns:
+            str: A string representation of the record.
+        """
 
         return (
             f"Invalid record"
